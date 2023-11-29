cf94c8854a1ba49a96413223e5883e420ab900b3
MinJi Choi <sistar96@gmail.com>
2023-11-29 12:03:49 +0900

    resolve, reject로 error 처리

--- Async_Test/main.js
+++ Async_Test/main.js
@@ -31,6 +31,87 @@ getMovies('frozen', ()=>{
 });
 */
 
+
+
+/* 정상적으로 처리 됐을 때
+const getMovies = (movieName) => {
+    return new Promise(resolve => {
+        fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                resolve();
+            })
+        });
+    }
+*/
+
+// getMovies('frozen').then(() => 
+//     {console.log("겨울 왕국")
+//     return getMovies('avengers');
+// }).then(() => {
+//     console.log("어벤져스")
+//     return getMovies('avatar');
+// }).then(() => 
+//     {console.log("아바타")});
+
+
+// 위 코드를 async, await 쓴 버전
+/*
+const wrap = async() => {
+    await getMovies('frozen')
+    console.log("겨울 왕국")
+    
+    await getMovies('avengers')
+    console.log("어벤져스")
+
+    await getMovies('avatar')
+    console.log("아바타")
+}
+wrap();
+*/
+
+// ****************** reject 버전 **************************
+/*
+const getMovies = (movieName) => {
+    return new Promise((resolve, reject) => {
+        fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json); 확인차
+                if(json.Response == "False"){
+                    reject(json.Error)
+                }
+
+                resolve(json);
+            })
+            .catch(err => {reject(err)})
+        });
+    }
+    
+// async 없이
+getMovies('avengers')
+    .then(movies => {console.log("영화 목록: ", movies)})
+    .catch(error => 
+        {
+            console.log("에러 발생: ", error)
+            loading = false;
+        })
+
+// async 로
+const wrap = async() => {
+    try{
+        const movies = await getMovies('avengers')
+        console.log("영화 목록 : ", movies)
+    }
+    catch(err){
+        console.log("에러 발생 : ", err)
+    }
+}
+wrap();
+*/
+
+// ************** 반복문에서 비동기 처리 *************** /
 const getMovies = (movieName) => {
     return new Promise(resolve => {
         fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
@@ -42,27 +123,19 @@ const getMovies = (movieName) => {
         });
     }
 
-/*
-getMovies('frozen').then(() => 
-    {console.log("겨울 왕국")
-    return getMovies('avengers');
-}).then(() => {
-    console.log("어벤져스")
-    return getMovies('avatar');
-}).then(() => 
-    {console.log("아바타")});
-*/
+const titles = ['frozen', 'avengers', 'avatar']
 
-// async, await 쓴 버전
-const wrap = async() => {
-    await getMovies('frozen')
-    console.log("겨울 왕국")
-    
-    await getMovies('avengers')
-    console.log("어벤져스")
+    /* *** 매번 순서가 달라서 삭제
+    titles.forEach(async title => {
+        const movies = await getMovies(title)
+        console.log(title, movies)
+    })
+    */
 
-    await getMovies('avatar')
-    console.log("아바타")
+const wrap = async()=>{
+    for(const title of titles){
+        const movies = await getMovies(title)
+        console.log(title, movies)
+    }
 }
-
 wrap();
--- Async_Test/main1.js
+++ Async_Test/main1.js
@@ -69,15 +69,15 @@ const c = () => {
 
 const d = () => {console.log(4);}
 
-/* 이전이랑 크게 달라보이지 않는다
-a().then(()=>{
-    b().then(()=>{
-        c().then(()=>{
-            d()
-        })
-    })
-})
-*/
+// 이전이랑 크게 달라보이지 않는다
+// a().then(()=>{
+//     b().then(()=>{
+//         c().then(()=>{
+//             d()
+//         })
+//     })
+// })
+
 
 /* 간단하게
 a().then(()=>{
@@ -109,7 +109,7 @@ a()
 
 
 // ************** Async, Await 패턴 *************** /
-
+/*
 const a = () => {
     return new Promise((resolve)=>{
         setTimeout(()=>{
@@ -121,10 +121,53 @@ const a = () => {
 
 const b = () => console.log(2);
 
-// a().then(()=>b()); 이 코드를 아래로 바꿀 수 있다
+//// a().then(()=>b()); 이 코드를 아래로 바꿀 수 있다
 
 const fun = async() => {
     await a()
     b()
 }
 fun()
+*/
+
+
+// ************** Resolve, Reject, error handling *************** /
+/*
+const delayAdd = (num) => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            if(num > 10){
+                reject(`${num}은 10보다 클 수 없다.`)
+                return
+            }
+
+            // 정상적으로 처리 됐을 때
+            console.log(num)
+            resolve(num + 1)
+            }, 1000)
+        }
+    );
+};
+*/
+
+
+// delayAdd(5)
+//     .then((res)=>{console.log(res)})
+//     .catch((err)=>{console.log(err)}); // 임의의 값, 함수, 함수
+
+/*
+const wrap = async() => {
+    try{
+    const res = await delayAdd(12)
+    console.log(res);
+    }
+    catch(err){
+        console.error(err)
+    }
+    finally{
+        console.log("Done!")
+    }
+}
+wrap();
+*/
+
