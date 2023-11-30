c5fbb33abfe9119803477b75f061719c1d96f8a4
MinJi Choi <sistar96@gmail.com>
2023-11-30 10:20:00 +0900

    RESTful 변수로 특정사용자 조회

--- Webserver_Test/express_server1.js
+++ Webserver_Test/express_server1.js
@@ -1,3 +1,4 @@
+// npm install express
 const express = require("express");
 
 // Data
@@ -18,11 +19,21 @@ const HOST = "0.0.0.0" // 모든 IP를 허락하겠다
 
 const app = express();
 app.get("/", (req, res)=>{
-    res.send("Hello World~~");
+    res.send("<h1>Hello World~~</h1>");
 });
 
 app.get("/users", (req, res)=>{
     res.send(Users);
+})
+
+/* 특정 사용자만 조회하겠다 */
+app.get("/users:userId", (req, res)=>{
+    const userId = req.params.userId // 가급적이면 변수이름 매칭 시켜주면 좋다
+    const user = Users[userId]
+    if(user)
+        res.send(user);
+    else    
+        res.sendStatus(404).end();
 })
 
 app.listen(PORT, HOST);
