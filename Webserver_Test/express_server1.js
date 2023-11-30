76e7849597bf993a39236efb6c1b6b2b52f8bfcf
MinJi Choi <sistar96@gmail.com>
2023-11-30 11:58:39 +0900

    MVC로 코드 분리

--- Webserver_Test/express_server1.js
+++ Webserver_Test/express_server1.js
@@ -1,17 +1,6 @@
 // npm install express
 const express = require("express");
-
-// Data
-const Users = [
-    {
-        id:0,
-        name: 'Jack'
-    },
-    {
-        id:1,
-        name: 'Jeniffer'
-    }
-]
+const userControllers = require("./controllers/users.controller");
 
 // CONSTANTS 포트, 서버 상수준비
 const PORT = 4000;
@@ -37,34 +26,9 @@ app.get("/", (req, res)=>{
     res.send("<h1>Hello World~~</h1>");
 });
 
-app.get("/users", (req, res)=>{
-    res.send(Users);
-})
-
-/* 특정 사용자만 조회하겠다 */
-app.get("/users:userId", (req, res)=>{
-    const userId = req.params.userId // 가급적이면 변수이름 매칭 시켜주면 좋다
-    const user = Users[userId]
-    if(user)
-        res.send(user);
-    else    
-        res.sendStatus(404).end();
-})
-
-app.post("/users", (req, res)=>{
-    //// console.log('req.body.name : ' + req.body.name);
-    /* 항상 꼼꼼하게 body가 없을 때 대비 */
-    if(!req.body.name){
-        return res.status(400).json({error: "Missing user name"}) // return으로 끊어주기
-    }
-
-    const newUser = {
-        id : Users.length, // 자동으로 추가되게끔}
-        name : req.body.name
-    }
-    Users.push(newUser);
-    res.json(newUser);
-});
+app.get("/users", userControllers.getUsers); /* 전체 회원 조회 */
+app.get("/users:userId", userControllers.getUser); /* 특정 사용자만 조회하겠다 */
+app.post("/users", userControllers.postUser);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
