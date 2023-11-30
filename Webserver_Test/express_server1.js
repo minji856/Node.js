78718842f69928d223db312337334a1211761464
MinJi Choi <sistar96@gmail.com>
2023-11-30 15:53:06 +0900

    path __dirname을 이용한 절대경로, hbs

--- Webserver_Test/express_server1.js
+++ Webserver_Test/express_server1.js
@@ -1,13 +1,14 @@
 // npm install express
 const express = require("express");
-const userControllers = require("./controllers/users.controller");
+const usersRouter = require("./routes/users.routes");
+const postsRouter = require("./routes/posts.routes");
+const path = require("path"); // 절대경로
 
 // CONSTANTS 포트, 서버 상수준비
 const PORT = 4000;
 const HOST = "0.0.0.0" // 모든 IP를 허락하겠다
 
 const app = express();
-// 서버 생성 후 MiddleWare
 
 /* 등록해주는 함수 */
 app.use((req, res, next)=>{
@@ -22,13 +23,25 @@ app.use((req, res, next)=>{
 // 미들웨어를 추가
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("<h1>Hello World~~</h1>");
-});
+// hbs middleWare 사용 설정
+app.set("view engine", 'hbs'); 
+app.set("views", path.join(__dirname, 'views'));
 
-app.get("/users", userControllers.getUsers); /* 전체 회원 조회 */
-app.get("/users:userId", userControllers.getUser); /* 특정 사용자만 조회하겠다 */
-app.post("/users", userControllers.postUser);
+// static을 추가하겠다
+app.use('/static', express.static(path.join(__dirname, 'public'))); 
+// http://localhost:4000/static/index.html
+
+
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+
+// 메인으로 접속할때만
+app.get("/", (req, res)=>{
+    res.render('index', {
+        imageTitle : "This is a Forest.....",
+        title: "Main Page"
+    }); // 어떤 파일을 그려줄지 render('파일명', {보내줄 데이터이름})
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
